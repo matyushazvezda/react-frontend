e78f5822cca5e08b59550ced84445bf3cd93910c
matyushazvezda <matyushazvezda@yandex.ru>
2023-05-25 01:27:04 +0800

    сортировка в списках концерта

--- src/components/Concerts.jsx
+++ src/components/Concerts.jsx
@@ -2,58 +2,64 @@ import React, { Component } from 'react';
 import ConcertsService from '../services/ConcertsService';
 
 function convertMillisecondsToDate(milliseconds) {
-    return new Date(milliseconds);
-  }
+  return new Date(milliseconds);
+}
 
 class Concerts extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            concerts: []
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      concerts: []
+    };
+  }
 
-    componentDidMount() {
-        ConcertsService.getConcerts().then((res) => {
-            this.setState({ concerts: res.data });
-        });
-    }
+  componentDidMount() {
+    this.fetchConcerts();
+  }
 
-    render() {
-        return (
-            <div className="container">
-                <h2 className="text-center">Список концертов</h2>
-                <div className="row justify-content-center">
-                    <div className="col-md-8">
-                        <table className="table table-striped table-bordered">
-                            <thead>
-                                <tr>
-                                    <th>Name</th>
-                                    <th>Location</th>
-                                    <th>Ticket Price (S)</th>
-                                    <th>Ticket Price (V)</th>
-                                    <th>Date</th>
-                                    <th>Time</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {this.state.concerts.map((concert) => (
-                                    <tr key={concert.id}>
-                                        <td>{concert.name}</td>
-                                        <td>{concert.location}</td>
-                                        <td>{concert.ticketPriceS}</td>
-                                        <td>{concert.ticketPriceV}</td>
-                                        <td>{convertMillisecondsToDate(concert.date).toLocaleDateString()}</td>
-                                        <td>{concert.time}</td>
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+  fetchConcerts() {
+    ConcertsService.getConcerts().then((res) => {
+      this.setState({ concerts: res.data });
+    });
+  }
+
+  render() {
+    const { concerts } = this.state;
+
+    return (
+      <div className="container">
+        <h2 className="text-center">Список концертов</h2>
+        <div className="row justify-content-center">
+          <div className="col-md-8">
+            <table className="table table-striped table-bordered">
+              <thead>
+                <tr>
+                  <th className="text-center">Название</th>
+                  <th className="text-center">Место проведения</th>
+                  <th className="text-center">Цена билета </th>
+                  <th className="text-center">Цена vip-билета</th>
+                  <th className="text-center">Дата</th>
+                  <th className="text-center">Время</th>
+                </tr>
+              </thead>
+              <tbody>
+                {concerts.map((concert) => (
+                  <tr key={concert.id}>
+                    <td className="text-center">{concert.name}</td>
+                    <td className="text-center">{concert.location}</td>
+                    <td className="text-center">{concert.ticketPriceS}</td>
+                    <td className="text-center">{concert.ticketPriceV}</td>
+                    <td className="text-center">{convertMillisecondsToDate(concert.date).toLocaleDateString()}</td>
+                    <td className="text-center">{concert.time}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Concerts;
